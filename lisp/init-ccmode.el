2c74088546a0be63f8065efa96fdec3d2a3628f8
Ayonga Hereid <ayonga27@gmail.com>
2018-10-23 21:54:33 -0400

    set cc tab-width

--- lisp/init-ccmode.el
+++ lisp/init-ccmode.el
@@ -6,6 +6,9 @@
 (define-key c++-mode-map (kbd "C-c C-c") 'compile)
 (define-key c++-mode-map (kbd "C-c C-k") 'kill-compilation)
 
+(defvaralias 'c-basic-offset 'tab-width)
+(defvaralias 'cperl-indent-level 'tab-width)
+
 ;;(add-to-list 'auto-mode-alist '("\\.cc$'" . c-mode)
 
 (provide 'init-ccmode)
