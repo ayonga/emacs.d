1e25781a0597f11f3ba6986c228af5ecddaba889
Ayonga Hereid <ayonga27@gmail.com>
2019-03-06 14:23:56 -0500

    c-basic-offset cannot be assinged to tab-width.

--- lisp/init-ccmode.el
+++ lisp/init-ccmode.el
@@ -6,8 +6,8 @@
 (define-key c++-mode-map (kbd "C-c C-c") 'compile)
 (define-key c++-mode-map (kbd "C-c C-k") 'kill-compilation)
 
-(defvaralias 'c-basic-offset 'tab-width)
-(defvaralias 'cperl-indent-level 'tab-width)
+;; (defvaralias 'c-basic-offset 'tab-width)
+;; (defvaralias 'cperl-indent-level 'tab-width)
 
 ;;(add-to-list 'auto-mode-alist '("\\.cc$'" . c-mode)
 
